c68ba55537e3cbab600d6ead528d87abf85b4af0
shirley.s <shirley.s@kakaocorp.com>
2020-08-20 11:53:29 +0900

    Updates

---
+++ precache-manifest.43ffdbc016b3643810279479308a86fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee18fe46f8ba9a605e9f010b7798fdbb",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "291ae40d26262a8f31ab",
+    "url": "/movie_app_2019/static/css/main.466a1f5f.chunk.css"
+  },
+  {
+    "revision": "fde87d2ad6cabe52f608",
+    "url": "/movie_app_2019/static/js/2.c3d73534.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2019/static/js/2.c3d73534.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "291ae40d26262a8f31ab",
+    "url": "/movie_app_2019/static/js/main.06aa4994.chunk.js"
+  },
+  {
+    "revision": "059a9ab2adfa282ae316",
+    "url": "/movie_app_2019/static/js/runtime-main.12568eff.js"
+  }
+]);
